e1a55d8205a22cac5f7548f0ccecdd15a015dac9
banuka <banukaramanayaka76@gmail.com>
2024-01-03 16:18:11 +0530

    initial:

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+/*
+*CONSIDER THE PIXEL DENSITY OF THE DEVICE  . NEED TO FIX FOR DIFFERENT DEVICES
+*/
+import Canvas from "./components/Canvas";// blue print for shaders
+function App(){
+  return (
+    <>
+       <Canvas/>
+       
+    </>
+  );
+}
+
+
+export default App;
